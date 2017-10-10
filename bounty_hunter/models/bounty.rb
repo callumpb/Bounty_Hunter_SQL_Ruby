24529b36fa3ae766e7caf820dab2f93cb2fe74d4
Callum <callumpb@hotmail.com>
2017-10-10 17:37:15 +0100

    added create method to bounty.rb

--- bounty_hunter/models/bounty.rb
+++ bounty_hunter/models/bounty.rb
@@ -10,3 +10,23 @@ class Bounty
     @bounty_value = options['bounty_value'].to_i
     @last_known_location = options['last_known_location']
   end
+
+  def create()
+    db = PG.connect({
+      dbname: 'bounties',
+      host: 'localhost'
+      })
+    sql = "
+    INSERT INTO bounties (name, species, bounty_value, last_known_location)
+    VALUES ($1, $2, $3, $4)
+    RETURNING *
+  "
+
+  values = [@name, @species, @bounty_value, @last_known_location]
+  db.prepare("create_bounty", sql)
+  #what it's calling, what its calling against?
+  @id = db.exec_prepared("create_bounty", values)[0]['id'].to_i
+  db.close()
+  end
+
+end
