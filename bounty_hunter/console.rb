ba19e7823857343f036adb2c56512623be0c4d3f
Callum <callumpb@hotmail.com>
2017-10-10 17:23:50 +0100

    console file added

---
+++ bounty_hunter/console.rb
@@ -0,0 +1,27 @@
+require('pry')
+require_relative('models/bounty.rb')
+
+Bounty.delete_all
+
+bounty_1 = Bounty.new(
+  {
+    'name' => 'Tony',
+    'species' => 'Portugese',
+    'bounty_value' => '1',
+    #bounty reduced due to bringing cookies
+    'last_known_location' => 'Codeclan'
+  }
+)
+
+bounty_2 = Bounty.new(
+  {
+    'name' => 'Darren',
+    'species' => 'Nohair',
+    'bounty_value' => '200000000',
+    #previous employers reclaiming lost money
+    'last_known_location' => 'Codeclan'
+  }
+)
+
+binding.pry
+nil
