46a2caea8ad50294a5d2cbdc08e045e8aa067c66
Callum <callumpb@hotmail.com>
2017-10-10 17:09:31 +0100

    sql file added

---
+++ bounty_hunter/models/bounty.rb
@@ -0,0 +1,12 @@
+require('pg')
+class Bounty
+  attr_accessor :name, :species, :bounty_value, :last_known_location
+  attr_reader :id
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+    @species = options['species']
+    @bounty_value = options['bounty_value'].to_i
+    @last_known_location = options['last_known_location']
+  end
